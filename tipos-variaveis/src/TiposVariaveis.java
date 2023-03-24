6588240fc3a9e54361caed4c5471e80fa386edc2
Igor Matheus <igmlcn.18@gmail.com>
2023-03-24 11:15:04 -0300

    commit sistema de uma SmartTv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
+
+/*tipos primivitos: int, byte, short, long(precisa ser encerrado com um L), boolean, char, float(precisa terminar com um F) e double;
+ */
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Igor Matheus";
+        
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero =5;
+        numero = 10;
+        System.out.println(numero);
+
+        /*Não pode mais modificar seu valor*/
+        final double VALOR_DE_PI = 3.14;
     }
 }
